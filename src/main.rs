0c48f58462d8924599baf6d94859f9b5045c3988
Kasey TIan <iamkaseytian@gmail.com>
2023-06-16 20:10:41 -0400

    Added help and item commands. Fixed receipt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,104 @@
+
+use crate::receipts::Receipt;
+use std::io::*;
 mod receipts;
 
 fn main() {
-    //test
+    let mut r = Receipt::new_with_name("test".to_string());
+    let tax = input_num("Enter a tax rate: ");
+    r.set_tax(tax);
+    let tip = input("Tip as value or as rate? ");
+    if tip.contains("r") {
+        let rate = input_num("Enter a tip rate: ");
+        r.set_tip_percent(rate);
+    }else if tip.contains("v") {
+        let tot = input_num("Enter receipt total: ");
+        let val = input_num("Enter tip amount: ");
+        r.set_tip_percent(val/tot);
+    }
+    loop {
+        let line = input("Enter a command: ");
+        let mut tokens = line.split(" ");
+        match tokens.next() {
+            Some("help") => help(),
+            Some("h") => help(),
+            Some("item") => item(&mut r,tokens.collect()),
+
+            _ => println!("Unrecognized command"),
+        }
+    }
+}
+fn input(text: &str) -> &str{
+    let mut input = String::new();
+    print!("{}", text);
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).expect("Did not enter a valid command");
+    if let Some('\n')=input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r')=input.chars().next_back() {
+        input.pop();
+    }
+    return input.as_str();
+}
+fn input_num(text: &str) -> f64 {
+    let line = input(text);
+    let res = line.parse::<f64>();
+    if res.is_err() {
+        println!("Error parsing number. Try again.");
+        return input_num(text);
+    }
+    let n = res.unwrap();
+    return n;
+}
+fn help() {
+    println!("Here is a list of available commands:");
+    println!("help - Print out this help message.");
+    println!("item [cost] [tax] [tip] - Add an item to your receipt. Cost should be the cost of the item, tax is y/n, tip is y/n.");
+
+}
+fn item(r: &mut Receipt, t: Vec<&str>) {
+    //name, cost, tax, tip
+    if t.len() != 4 {
+        println!("Incorrect number of parameters supplied");
+        return;
+    }
+    //parse name
+    let name = t[0];
+
+    //parse cost
+    if t[1].parse::<u32>().is_err() {
+        println!("Error parsing cost");
+        return;
+    }
+    let cost:u32 = t[1].parse().unwrap();
+
+    //parse tax
+    if text_to_bool(t[2]).is_none() {
+        println!("Error parsing tax");
+        return;
+    }
+    let tax = text_to_bool(t[2]).unwrap();
+
+    //parse tip
+    if text_to_bool(t[3]).is_none() {
+        println!("Error parsing tip");
+        return;
+    }
+    let tip = text_to_bool(t[3]).unwrap();
+
+    r.add_item(name.to_string(), cost, tax, tip);
+    println!("Item added successfully");
+}
+
+fn text_to_bool(input: &str) -> Option<bool>{
+    let c = input.chars().next();
+    if c.is_none() {
+        return None;
+    }
+    match c.unwrap() {
+        'Y' | 'y' | 'T' | 't' => Some(true),
+        'N' | 'n' | 'F' | 'f' => Some(false),
+        _ => None
+    }
 }
--- src/receipts.rs
+++ src/receipts.rs
@@ -3,21 +3,159 @@ use std::fmt;
 use std::ops::*;
 use std::hash::{Hash, Hasher};
 
-pub struct Receipt {
-    people: Vec<Person>,
-    items: Vec<Item>
+pub struct Receipt<'a> {
+    name: String,
+    people: Vec<Person<'a>>,
+    items: Vec<Item<'a>>,
+    tax_rate: f64,
+    tip_rate:f64
+}
+impl Receipt<'_> {
+    pub fn new() -> Self {
+        return Self {
+            name: "".to_string(),
+            people: Vec::new(),
+            items: Vec::new(),
+            tax_rate: 1.0,
+            tip_rate: 1.0,
+        }
+    }
+    pub fn new_with_name(name: String) -> Self {
+        return Self {
+            name,
+            people: Vec::new(),
+            items: Vec::new(),
+            tax_rate: 1.0,
+            tip_rate: 1.0,
+        }
+    }
+    pub fn add_person(&mut self, name: String) -> bool{
+        let p = Person::new_with_name(name);
+        if !self.people.contains(&p) {
+            self.people.push(p);
+            return true;
+        }
+        return false;
+    }
+    pub fn add_item(&mut self, name: String, cost: u32, tax: bool, tip: bool) -> bool {
+        let i = Item::new_with_all(
+            name,
+            cost as f64,
+            if tax {self.tax_rate} else {1.0},
+            if tip {self.tip_rate} else {1.0});
+        if !self.items.contains(&i) {
+            self.items.push(i);
+            return true;
+        }
+        return false;
+    }
+    pub fn set_tax(&mut self, r:f64) {
+        self.tax_rate = r;
+        for i in self.items.iter_mut() {
+            if i.tax {
+                i.tax_rate = r;
+            }
+        }
+    }
+    pub fn set_tip_percent(&mut self, r: f64) {
+        self.tip_rate = r;
+        for i in self.items.iter_mut() {
+            if i.tip {
+                i.tip_rate = r;
+            }
+        }
+    }
+    pub fn set_share(&mut self, person: usize, item: usize, share:f64) {
+        self.items[item].set_share(&self.people[person], &share);
+        self.people[person].add_item(&self.items[item]);
+    }
+    pub fn get_totals(&self) -> HashMap<String, u32> {
+        let mut map = HashMap::new();
+        for p in self.people.iter() {
+            map.insert(p.name.clone(), p.total().0 as u32);
+        }
+        return map;
+    }
+    fn total_paid(&self) -> Cost {
+        let mut sum = Cost(0.0);
+        for p in self.people.iter() {
+            sum += p.total();
+        }
+        return sum;
+    }
+    fn total_cost(&self) -> Cost {
+        let mut sum = Cost(0.0);
+        for i in self.items.iter() {
+            sum += i.total();
+        }
+        return sum;
+    }
+    fn sub_total(&self) -> Cost {
+        let mut sum = Cost(0.0);
+        for i in self.items.iter() {
+            sum += i.cost.clone();
+        }
+        return sum;
+    }
+    fn total_tip(&self) -> Cost {
+        let mut t = Cost(0.0);
+        for i in self.items.iter() {
+            t += i.total_tip();
+        }
+        return t;
+    }
+    fn total_tax(&self) -> Cost {
+        let mut t = Cost(0.0);
+        for i in self.items.iter() {
+            t += i.total_tax();
+        }
+        return t;
+    }
+    pub fn to_string(&self) -> String {
+        let mut str = String::new();
+        str += &self.name;
+        str += "\n";
+        for i in 0..self.items.len() {
+            str += &i.to_string();
+            str += ". ";
+            str += &self.items[i].to_string();
+        }
+        str += "\n";
+        for i in 0..self.people.len() {
+            str += &i.to_string();
+            str += ". ";
+            str += &self.people[i].to_string();
+        }
+        str += "\nSubtotal: ";
+        str += &self.sub_total().to_string();
+        str += "\nTax: ";
+        str += &self.total_tax().to_string();
+        str += "\nTip: ";
+        str += &self.total_tip().to_string();
+        str += "\nTotal: ";
+        str += &self.total_cost().to_string();
+        str += "\nPaid: ";
+        str += &self.total_paid().to_string();
+        return str;
+    }
+}
+
+impl fmt::Display for Receipt<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return write!(f, "{}", self.to_string());
+    }
 }
 
 #[derive(Clone)]
-pub struct Person {
+struct Person<'a> {
     name: String,
-    items: Vec<Item>,
+    items: Vec<&'a Item<'a>>,
 }
-impl Person{
-    pub fn new() -> Self {
+impl Person<'_>{
+    fn new() -> Self {
         return Self::new_with_name("".to_string());
     }
-    pub fn new_with_name(name: String) -> Self {
+    fn new_with_name(name: String) -> Self {
         return Self {
             name,
             items: Vec::new()
@@ -30,57 +168,77 @@ impl Person{
         }
         return tot;
     }
-    pub fn to_string(&self) -> String{
+    fn to_string(&self) -> String{
         let mut str = String::new();
         str += &self.name;
-        str += "\n";
+        str += "(";
+        str += &self.total().to_string();
+        str += ")\n";
         for i in self.items.iter() {
             str += &i.name;
-            str += ", ";
+            str += "(";
+            str += &i.cost_per_person(self).to_string();
+            str += "), ";
         }
         return str;
     }
+    fn add_item(&mut self, i: &Item) {
+        if !self.items.contains(&i) {
+            self.items.push(&i);
+        }
+    }
 }
-impl PartialEq for Person {
+impl fmt::Display for Person<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return write!(f, "{}", self.to_string());
+    }
+}
+impl PartialEq for Person<'_> {
     fn eq(&self, other: &Self) -> bool {
         return self.name == other.name;
     }
 }
-impl Eq for Person {}
-impl Hash for Person {
+impl Eq for Person<'_> {}
+impl Hash for Person<'_> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.name.hash(state);
     }
 }
 
 #[derive(Clone)]
-pub struct Item {
+struct Item<'a> {
     name: String,
     cost: Cost,
-    shares: HashMap<Person, f64>,
+    shares: HashMap<&'a Person<'a>, f64>,
     tax_rate: f64,
     tip_rate: f64,
+    tax: bool,
+    tip: bool
 }
-impl Item {
-    pub fn new() -> Self {
+impl Item<'_> {
+    fn new() -> Self {
         return Self::new_with_all(String::new(), 0.0,1.0, 1.0);
     }
-    pub fn new_with_name_cost(name: String, cost: f64) -> Self {
+    fn new_with_name_cost(name: String, cost: f64) -> Self {
         return Self::new_with_all(name, cost, 1.0, 1.0);
     }
-    pub fn new_with_all(name: String, cost: f64, tax_rate: f64, tip_rate: f64) -> Self {
+    fn new_with_all(name: String, cost: f64, tax_rate: f64, tip_rate: f64) -> Self {
+        let tax = tax_rate != 1.0;
+        let tip = tip_rate != 1.0;
         return Self {
             name,
             cost: Cost(cost),
             shares: HashMap::new(),
             tax_rate,
-            tip_rate
+            tip_rate,
+            tax,
+            tip,
         }
     }
-    pub fn set_share(&mut self, p: &Person, s: &f64) {
-        self.shares.insert(p.clone(), s.clone());
+    fn set_share(&mut self, p: &Person, s: &f64) {
+        self.shares.insert(p, s.clone());
     }
-    pub fn total(&self) -> Cost{
+    fn total(&self) -> Cost{
         let multiplier = self.tax_rate * self.tip_rate;
         return self.cost.clone() * Cost(multiplier);
     }
@@ -92,13 +250,19 @@ impl Item {
         let share = Cost(*self.shares.get(p).unwrap());
         return self.cost_per_share() * share;
     }
-    fn all_costs_per_person(&self) -> HashMap<Person, Cost> {
+    fn all_costs_per_person(&self) -> HashMap<&Person, Cost> {
         let mut map = HashMap::new();
         let per_share = self.cost_per_share();
         for p in self.shares.iter() {
-            map.insert((*p.0).clone(), per_share.clone() * Cost(*p.1));
+            map.insert(*p.0, per_share.clone() * Cost(*p.1));
         }
         return map;
+    }
+    fn total_tax(&self) -> Cost {
+        return self.cost.clone() * Cost(self.tax_rate - 1.0);
+    }
+    fn total_tip(&self) -> Cost {
+        return self.total() - self.cost.clone() - self.total_tax();
     }
     fn to_string(&self) -> String{
         let mut str = String::new();
@@ -111,16 +275,27 @@ impl Item {
             str += &i.0.name;
             str += "(";
             str += &i.1.to_string();
+            str += ",";
+            str += &self.cost_per_person(i.0).to_string();
             str += "), ";
         }
         return str;
     }
 }
-impl fmt::Display for Item {
+impl fmt::Display for Item<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         return write!(f, "{}", self.to_string());
     }
 }
+impl PartialEq for Item<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        return self.name == other.name
+        && self.tip_rate == other.tip_rate
+        && self.tax_rate == other.tax_rate
+        && self.cost == other.cost;
+    }
+}
+impl Eq for Item<'_> {}
 
 struct Cost (f64);
 impl Cost {
